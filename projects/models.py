8883b99611242785e847ab38f77633d1c7908674
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2023-05-15 19:20:20 +0300

    create Review and Tag models

--- projects/models.py
+++ projects/models.py
@@ -8,9 +8,37 @@ class Project(models.Model):
     description = models.TextField(null=True, blank=True)
     liveLink = models.CharField(max_length=2000, null=True, blank=True)
     codeLink = models.CharField(max_length=2000, null=True, blank=True)
+    tags = models.ManyToManyField('Tag', blank=True)
+    voteTotal = models.IntegerField(default=0, null=True, blank=True)
+    voteRatio = models.IntegerField(default=0, null=True, blank=True)
     createdAt = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True,
                           primary_key=True, editable=False)
 
     def __str__(self):
         return self.title
+
+class Review(models.Model):
+    VOTE_TYPE = (
+        ('up', 'Up Vote'),
+        ('down', 'Down Vote'),
+    )
+    # owner = 
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    body = models.TextField(null=True, blank=True)
+    value = models.CharField(max_length=200)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.value
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.name
