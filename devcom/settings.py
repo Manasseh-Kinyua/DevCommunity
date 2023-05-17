9b6ab9b415ffa7637d539c06d03d33701ef8c095
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2023-05-17 20:00:30 +0300

    install whitenoise

--- devcom/settings.py
+++ devcom/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-kl3phd+!-zttr7gly*=&m9#e&gig6evrkajm*#_$&7^)-)rj^%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -44,6 +44,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -128,6 +131,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
