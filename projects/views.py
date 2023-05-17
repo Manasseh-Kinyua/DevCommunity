f95a7813c7d91f1c4f4da58cb4ed5e266eb97063
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2023-05-17 08:52:48 +0300

    create project model form

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Project
+from .forms import ProjectForm
 
 # Create your views here.
 
@@ -17,3 +18,10 @@ def project(request, pk):
         'project': project
     }
     return render(request, 'projects/single-project.html', context)
+
+def createProject(request):
+    form = ProjectForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'projects/project_form.html', context)
