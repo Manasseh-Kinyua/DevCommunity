e91d4245fe88a21b68ed0f50aae5a758579ff387
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2023-05-19 19:46:11 +0300

    manage top skills and other skills

--- users/views.py
+++ users/views.py
@@ -12,7 +12,13 @@ def profiles(request):
 
 def profile(request, pk):
     profile = Profile.objects.get(id=pk)
+
+    topSkills = profile.skill_set.exclude(description__exact="")
+    otherSkills = profile.skill_set.filter(description="")
+
     context = {
-        'profile': profile
+        'profile': profile,
+        'topSkills': topSkills,
+        'otherSkills': otherSkills,
     }
     return render(request, 'users/user-profile.html', context)
