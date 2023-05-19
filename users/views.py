c90daea4a383c39650dcc99ee40f18fc858b86a4
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2023-05-19 19:11:58 +0300

    profile page

--- users/views.py
+++ users/views.py
@@ -9,3 +9,10 @@ def profiles(request):
         'profiles': profiles
     }
     return render(request, 'users/profiles.html', context)
+
+def profile(request, pk):
+    profile = Profile.objects.get(id=pk)
+    context = {
+        'profile': profile
+    }
+    return render(request, 'users/user-profile.html', context)
