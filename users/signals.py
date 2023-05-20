344b9cc10500a9846ab7a1193c54a9aafe1f6169
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2023-05-20 21:46:28 +0300

    generate user account using signals

--- users/signals.py
+++ users/signals.py
@@ -1,7 +1,19 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
+from django.contrib.auth.models import User
 from .models import Profile
 
-def profileUpdate(sender, instance, created, **kwargs):
-    print("Profile Updated!")
+def createProfile(sender, instance, created, **kwargs):
+    if created:
+        user = instance
+        profile = Profile.objects.create(
+            user = user,
+            username = user.username,
+            email = user.email,
+            name = user.first_name
+        )
 
-post_save.connect(profileUpdate, sender=Profile)
+def profileDeleted(sender, instance, **kwargs):
+    print("User Deleted!")
+
+post_save.connect(createProfile, sender=User)
+post_delete.connect(profileDeleted, sender=Profile)
