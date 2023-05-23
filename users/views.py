266630c9b26b608f9f79c4d5fc2e9968c0b1a653
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2023-05-23 19:18:13 +0300

    use signals to update user info from the profile

--- users/views.py
+++ users/views.py
@@ -83,7 +83,15 @@ def profile(request, pk):
 
 @login_required(login_url='login')
 def editProfile(request):
-    form = ProfileForm()
+    profile = request.user.profile
+    form = ProfileForm(instance=profile)
+
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            form.save()
+
+            return redirect('account')
 
     context = {
         'form': form
